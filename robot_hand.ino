9f0115f38029cc3be17be417115fd5e3df46f36c
K1tsch01 <imtuma1103@gmail.com>
2026-07-14 00:44:09 +0900

    Arduino 내부 코드 수정 (LED -> 서보 모터)

--- robot_hand.ino
+++ robot_hand.ino
@@ -1,9 +1,16 @@
 #include <Servo.h>
 
+
+Servo servos[6];
+// 순서대로, 엄 검 중 약 소 / 손목
+int pins[6] = {3, 5, 6, 9, 10, 11};
+bool prev_fingers[5] = {false, false, false, false, false};
+bool same;
+
 void setup() {
-  // put your setup code here, to run once:
-  for (int i = 2; i < 7; i++) {
-    pinMode(i, OUTPUT);
+  
+  for (int i = 0; i < 6; i++) {
+    servos[i].attach(pins[i]);
   }
 
   Serial.begin(9600);
@@ -12,18 +19,31 @@ void setup() {
 void loop() {
   if (Serial.available()) {
     String data = Serial.readStringUntil('\n');
-    bool fingers[] = {false , false , false , false , false};
+    same = true;
+    bool fingers[5];
+    
+    // 이전 꺼랑 같은지 비교
     for (int i = 0; i < 5; i++) {
       fingers[i] = (data[i] == '1');
-    }
-
-    for (int i = 2; i < 7; i++) {
-      if (fingers[i - 2]) {
-        digitalWrite(i, HIGH);
-      } else {
-        digitalWrite(i , LOW);
+      if (prev_fingers[i] != fingers[i]) {
+        same = false;
       }
     }
+    
+    // 같으면 더 연산 안함
+    if (same) {
+      return;
+    }
+
+    // 비교 대상 갱신
+    for (int i = 0; i < 5; i++) {
+      prev_fingers[i] = fingers[i];
+    }
+
+    for (int i = 0; i < 5; i++) {
+      servos[i].write(fingers[i] ? 180 : 0);
+    }
+    delay(20);
   } 
 }
   
